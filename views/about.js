388c4b25fd164d9965846abf03c531be530eaad1
AndreiOiegar <andrei.oiegar@gmail.com>
2020-07-30 20:15:50 +0300

    add

--- views/about.js
+++ views/about.js
@@ -1,27 +1,63 @@
-const arrowDown = document.querySelector(".ed-arrow-down");
-const arrowRight = document.querySelector(".ed-arrow-right")
-const educationInfo = document.querySelector(".ed-content");
-console.log(arrowDown)
+function education(){
+    const arrowDown = document.querySelector(".ed-arrow-down");
+    const arrowRight = document.querySelector(".ed-arrow-right")
+    const educationInfo = document.querySelector(".ed-content");
+    console.log(arrowDown)
 
-arrowDown.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log(educationInfo)
-    educationInfo.style.display = "flex";
-    changeArrows()
-})
+    arrowDown.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log(educationInfo)
+        educationInfo.style.display = "flex";
+        changeEdArrows()
+    })
 
-arrowRight.addEventListener("click", (event) => {
-    event.preventDefault();
-    educationInfo.style.display = "none";
-    changeArrows()
-})
+    arrowRight.addEventListener("click", (event) => {
+        event.preventDefault();
+        educationInfo.style.display = "none";
+        changeEdArrows()
+    })
 
-function changeArrows(){
-    if(educationInfo.style.display === "flex"){
-        arrowDown.style.display = "none";
-        arrowRight.style.display = "flex";
-    } else {
-        arrowDown.style.display = "flex";
-        arrowRight.style.display = "none";
+    function changeEdArrows() {
+        if (educationInfo.style.display === "flex") {
+            arrowDown.style.display = "none";
+            arrowRight.style.display = "flex";
+        } else {
+            arrowDown.style.display = "flex";
+            arrowRight.style.display = "none";
+        }
     }
 }
+
+education();
+
+function experience(){
+    const arrowDown = document.querySelector(".ex-arrow-down");
+    const arrowRight = document.querySelector(".ex-arrow-right")
+    const experienceInfo = document.querySelector(".experience-content");
+    console.log(arrowDown)
+
+    arrowDown.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log(experienceInfo)
+        experienceInfo.style.display = "flex";
+        changeExArrows()
+    })
+
+    arrowRight.addEventListener("click", (event) => {
+        event.preventDefault();
+        experienceInfo.style.display = "none";
+        changeExArrows()
+    })
+
+    function changeExArrows() {
+        if (experienceInfo.style.display === "flex") {
+            arrowDown.style.display = "none";
+            arrowRight.style.display = "flex";
+        } else {
+            arrowDown.style.display = "flex";
+            arrowRight.style.display = "none";
+        }
+    }
+}
+
+experience()
